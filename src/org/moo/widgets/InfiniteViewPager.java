8dfb1fbe908dff625b2df5a92c657f2161eef86d
Casey Borders <TheBeast.13@gmail.com>
2013-12-29 13:43:39 -0500

    Allow wrap_contents

--- src/org/moo/widgets/InfiniteViewPager.java
+++ src/org/moo/widgets/InfiniteViewPager.java
@@ -1671,7 +1671,43 @@ public class InfiniteViewPager extends ViewGroup
 				}
 			}
 		}
+		
+		// find the first child view
+        View view = getChildAt(0);
+        if (view != null) {
+            // measure the first child view with the specified measure spec
+            view.measure(widthMeasureSpec, heightMeasureSpec);
+        }
+
+        setMeasuredDimension(getMeasuredWidth(), measureHeight(heightMeasureSpec, view));
 	}
+	
+	/**
+     * Determines the height of this view
+     *
+     * @param measureSpec A measureSpec packed into an int
+     * @param view the base view with already measured height
+     *
+     * @return The height of the view, honoring constraints from measureSpec
+     */
+    private int measureHeight(int measureSpec, View view) {
+        int result = 0;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+
+        if (specMode == MeasureSpec.EXACTLY) {
+            result = specSize;
+        } else {
+            // set the height from the base view if available
+            if (view != null) {
+                result = view.getMeasuredHeight();
+            }
+            if (specMode == MeasureSpec.AT_MOST) {
+                result = Math.min(result, specSize);
+            }
+        }
+        return result;
+    }
 
 	@Override
 	protected void onSizeChanged(int w, int h, int oldw, int oldh)
